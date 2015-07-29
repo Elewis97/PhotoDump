1cd170a3f0bf63414760c88998ee459ba90e90bf
Kiet <kietmsm@gmail.com>
2015-07-29 11:08:41 -0700

    did backend stuff kiet

--- main.py
+++ main.py
@@ -64,25 +64,11 @@ class NewsfeedHandler(webapp2.RequestHandler):
         # a model in datastore. If not, then create a User model for the current_user
         # and add it into datastore.
 
-        temp_user_model = User(user=current_user)
-
-        added = False
-        for user in user_list:
-            if user.user == current_user:
-                added = True
-                current_user_model = user
-        if not added:
-            temp_user_model.put()
-            current_user_model = temp_user_model
-
-        def get_user_model():
-            current_user = users.get_current_user()
-            user_model_list = User.query().fetch()
-            
-            return current_user_model
-
-        greeting = current_user.nickname()
-        template_vars = {"photo_group_data" : current_user_model.photo_groups, "greeting" : greeting}
+        current_user = get_user_model()
+        logging.info(current_user)
+        logging.info(current_user.user.nickname())
+        greeting = current_user.user.nickname()
+        template_vars = {"photo_group_data" : [], "greeting" : greeting}
         self.response.write(template.render(template_vars))
 
 #This handler is needed in order to create a group.
@@ -204,7 +190,13 @@ class TestHandler(webapp2.RequestHandler):
         #self.response.write(group.photos)
         #self.response.write("DISLIKES: " + str(group.dislikes))
 
-
+def get_user_model():
+    current_user = users.get_current_user()
+    user_model_list = User.query().fetch()
+    for temp_user_model in user_model_list:
+        if temp_user_model.user == current_user:
+            return temp_user_model
+    return "none"
 
 jinja2_environment = jinja2.Environment(loader=
     jinja2.FileSystemLoader(os.path.dirname(__file__)))
